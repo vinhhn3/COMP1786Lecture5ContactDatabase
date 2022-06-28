f7039d2e9bdfd478f5f1fa14555ca7afb1db47cc
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-06-28 15:11:00 +0700

    Add DatabaseHelper Class

---
+++ app/src/main/java/com/example/comp1786lecture5contactdatabase/DatabaseHelper.java
@@ -0,0 +1,46 @@
+package com.example.comp1786lecture5contactdatabase;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "details";
+
+    public static final String ID_COLUMN = "person_id";
+    public static final String NAME_COLUMN = "name";
+    public static final String DOB_COLUMN = "dob";
+    public static final String EMAIL_COLUMN = "email";
+
+    private SQLiteDatabase database;
+
+    private static final String DATABASE_CREATE = String.format(
+            "CREATE TABLE %s (" +
+                    "%s INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "%s TEXT, " +
+                    "%s TEXT, " +
+                    "%s TEXT)",
+            DATABASE_NAME, ID_COLUMN, NAME_COLUMN, DOB_COLUMN, EMAIL_COLUMN
+    );
+
+    public DatabaseHelper(Context context){
+        super(context, DATABASE_NAME, null, 1);
+        database = getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + DATABASE_NAME);
+
+        Log.v(this.getClass().getName(), DATABASE_NAME +
+                "database upgrade to version" + newVersion + " - old data lost"
+        );
+        onCreate(db);
+    }
+}
