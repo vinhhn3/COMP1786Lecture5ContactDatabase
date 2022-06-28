2152d2ee9968decfa6f4592656a2f6c088ad0f06
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-06-28 15:25:43 +0700

    Get data from form and insert to database

--- app/src/main/java/com/example/comp1786lecture5contactdatabase/MainActivity.java
+++ app/src/main/java/com/example/comp1786lecture5contactdatabase/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.comp1786lecture5contactdatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button saveDetailsButton = findViewById(R.id.saveDetailsButton);
+
+        saveDetailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveDetails();
+            }
+        });
     }
+
+    private void saveDetails(){
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+
+        EditText nameTxt = findViewById(R.id.nameText);
+        EditText dobTxt = findViewById(R.id.dobText);
+        EditText emailTxt = findViewById(R.id.emailText);
+
+        String name = nameTxt.getText().toString();
+        String dob = dobTxt.getText().toString();
+        String email = emailTxt.getText().toString();
+
+        long personId = dbHelper.insertDetails(name, dob, email);
+
+        Toast.makeText(this, "Person has been created with id: " + personId,
+                Toast.LENGTH_LONG
+        ).show();
+    }
+
+
 }
